ba54d3e504301f28d13a425b7069a8caa18aaf92
Dipsaint1 <ayodejioladapo96@gmail.com>
2022-06-22 21:07:02 +0100

    Added HeroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,25 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './HeroSection.css';
 import Button from "./utils/Button";
-import video from "../videos/video.mp4";
+// import video from "../videos/video.mp4";
 import img from "../images/man-custom-suit.jpg";
-
 
 
 function HeroSection() {
   return (
     <div className="hero-container">
       <img className="hero-image" src={img} alt="" />
-      <video src={video} autoPlay muted loop></video>
+      {/* <video src={video} autoPlay muted loop></video> */}
       <div className="hero-information">
-        <h1 className="hero-header">Cutting and Tailoring</h1>
-        <p className="hero-text">Each shirt isi cut individually in order to match the pattern of the fabric from yoke to sleeve as well as at the sleeve plackets</p>
+        <h1 className="hero-header">Cutting <br/> <span> & Tailoring</span></h1>
+        <p className="hero-text">Each shirt is cut individually in order to match the pattern of the fabric from yoke to sleeve as well as at the sleeve plackets</p>
       </div>
       <div className='hero-footer'>
-        <span> Facebook </span>
-        <span> Twitter </span>
-        <span> Instagram </span>
-        <span> TikTok </span>
+        <Link to="#"><span> Facebook </span> </Link>
+        <Link to="#"><span> Twitter </span> </Link>
+        <Link to="#"><span> Instagram </span> </Link>
+        <Link to="#"><span> TikTok </span> </Link>
       </div>
     </div>
   )
