153eb362d9dbdaa41c75fb7d01d59e12d280f92d
Cyber9ja <ayodejioladapo96@gmail.com>
2023-02-04 09:43:29 +0000

    updated navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,11 +12,22 @@ const Navbar = () => {
           <img className='scissor' src={Scissor} alt="scissor" />
           <h4 className='header-text'>Exquisite Couture</h4>
         </div>
-        <div className="menu-bars">
-
+        <div className="menu-bars-container">
+          <div className="menu-bars">
+            <span className="bars"></span>
+            <span className="bars"></span>
+            <span className="bars"></span>
+          </div>
         </div>
         <nav>
-
+          <ul>
+            <Link to='' className="nav-item">Home</Link>
+            <Link to='' className="nav-item">About</Link>
+            <Link to='' className="nav-item">Services</Link>
+            <Link to='' className="nav-item">Portfolio</Link>
+            <Link to='' className="nav-item">Contact</Link>
+            <p className="nav-item phone">08162845828</p>
+          </ul>
         </nav>
       </header>
   )
