d8ba0ce9f9943342e6a172809c6326f7d1868124
Cyber9ja <ayodejioladapo96@gmail.com>
2023-02-06 09:53:27 +0000

    added hero section image

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,14 +4,18 @@ import Carousel from "./carousel/Carousel";
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
+import Hero1 from '../images/hero1.jpg';
+import Hero2 from '../images/hero2.jpg';
+
 
 function HeroSection() {
   return (
     <div className='hero-section'>
       <Carousel />
       <main id='main'>
-        <div className="left">
-          <h1>Your Personal Tailor For Unique People Only</h1>
+        <div className="container">
+          <div className="left">
+            <h1>Your Personal Tailor For Unique People Only</h1>
             <h2>Welcome to <span>Exquisite Couture</span></h2>
             <span></span>
             <h4>Why Custom Tailoring</h4>
@@ -22,9 +26,11 @@ function HeroSection() {
             <div className="service-info-container">
               <Link className='service-info-link' to=''><button>Services Info <FontAwesomeIcon className='icon' icon={faAnglesRight} /> </button></Link>
             </div>
-        </div>
-        <div className="right">
-          
+          </div>
+          <div className="right">
+            <img className='hero-img hero-img-1' src={Hero1} alt="hero-img" />
+            <img className='hero-img hero-img-2' src={Hero2} alt="hero-img" />
+          </div>
         </div>
         
       </main>
